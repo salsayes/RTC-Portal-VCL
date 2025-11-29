b6525aa69db3defcf4f1e5d25b6532b5b2ff1d78
Salsayes <salsayes90@gmail.com>
2025-11-28 22:15:05 -0300

    Add TAutoUpdater component for silent client updates

--- Lib/rtcPortalReg.pas
+++ Lib/rtcPortalReg.pas
@@ -26,7 +26,7 @@ uses
   rtcpDesktopHost, rtcpRemoteShell, rtcpRemoteCamVCL, rtcpRemoteFileManager,
   rtcpDesktopControl, rtcpDesktopControlUI,
   rtcpFileExplore,
-  rtcpCustomComm,
+  rtcpCustomComm, rtcpAutoUpdater,
   rtcPortalGate,
   Classes;
 
@@ -36,7 +36,7 @@ begin
     TRtcPFileTransfer, TRtcPFileTransferUI, TRtcPChat, TRtcPChatUI,
     TRtcPDesktopHost, TRtcPDesktopControl, TRtcPDesktopControlUI,
     TRtcPDesktopViewer, TRtcPFileExplorer, TRtcPCustomCommand, TRtcPRemoteShell,
-    TRemoteCamVCL, TRtcPRemoteFileManager,
+    TRemoteCamVCL, TRtcPRemoteFileManager, TAutoUpdater,
     TRtcPortalGateway]);
 end;
 
